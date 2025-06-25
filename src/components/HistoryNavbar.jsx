ddfb2b43909352c535414a45ab7b5083072c8e8d
DESKTOP-3I4UA3R\AREGBESOLA .BOLU <justicearegbesola@gmail.com>
2025-06-25 16:03:01 +0100

    history navbar

--- src/components/HistoryNavbar.jsx
+++ src/components/HistoryNavbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./HistoryNavbar.css";
+import { LeftOutlined } from "@ant-design/icons";
+import { HomeOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 const HistoryNavbar = () => {
   return (
-    <div className="navbar">
-      <nav className="nav">
-        <div>Transaction History</div>
-        <div>@</div>
+    <div>
+      <nav className="navbar">
+        <div className="nav">
+          <Link to="/home">
+            <div>
+              <LeftOutlined />
+            </div>
+          </Link>
+          <div>Transaction History</div>
+          <Link to="/home">
+            <div>
+              <HomeOutlined />
+            </div>
+          </Link>
+        </div>
       </nav>
     </div>
   );
